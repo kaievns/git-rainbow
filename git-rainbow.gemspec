28678c7e3d59269a21f4130a6884a60d29584f7a
Nikolay Nemshilov <nemshilov@gmail.com>
2015-10-24 14:28:42 +1100

    [38;5;161mM[0m[38;5;196ma[0m[38;5;202mk[0m[38;5;208mi[0m[38;5;214mn[0m[38;5;220mg[0m[38;5;184m [0m[38;5;155mt[0m[38;5;120mh[0m[38;5;85me[0m[38;5;50m [0m[38;5;80mp[0m[38;5;39ma[0m[38;5;33mi[0m[38;5;27mn[0m[38;5;21mt[0m[38;5;20me[0m[38;5;55mr[0m[38;5;90m [0m[38;5;125mt[0m[38;5;161mo[0m[38;5;196m [0m[38;5;202ms[0m[38;5;208mp[0m[38;5;214mi[0m[38;5;220mt[0m[38;5;184m [0m[38;5;155mo[0m[38;5;120mu[0m[38;5;85mt[0m[38;5;50m [0m[38;5;80mr[0m[38;5;39ma[0m[38;5;33mi[0m[38;5;27mn[0m[38;5;21mb[0m[38;5;20mo[0m[38;5;55mw[0m[38;5;90ms[0m[38;5;125m![0m

--- git-rainbow.gemspec
+++ git-rainbow.gemspec
@@ -18,5 +18,6 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "tco", "~> 0.1.8"
   spec.add_development_dependency "bundler", "~> 1.9"
 end
--- lib/git_rainbow/painter.rb
+++ lib/git_rainbow/painter.rb
@@ -1,17 +1,31 @@
+require "tco"
+
 module GitRainbow::Painter
   extend self
 
   def paint(message)
     palette = build_palette(message.size)
-
-    message.chars.map { |ch| colorize(char, palette.shift) }.join("")
-  end
-
-  def build_palette(size)
-    []
+    message.chars.map { |ch| colorize(ch, palette.shift) }.join("")
   end
 
   def colorize(char, color)
-    "\e[32m#{char}\e[0m"
+    Tco.fg(color, char)
+  end
+
+  def build_palette(size)
+    size.times.map do |i|
+      # we're basically generating a bunch of sine wave sequences
+      # with a PI/2 phase shift between them to represent RGB colors
+      red   = wave_value_at(i, size: size, phase:  Math::PI/2 - 1)
+      green = wave_value_at(i, size: size, phase:  0          - 1)
+      blue  = wave_value_at(i, size: size, phase: -Math::PI/2 - 1)
+      "##{red}#{green}#{blue}"
+    end
+  end
+
+  def wave_value_at(i, size: 1, phase: 0)
+    sin = Math.sin(Math::PI / size * 4 * i + phase) # pure sin -1..+1 value
+    int = sin * 127 + 128 # 0..255 value
+    "%02x" % int # 2 symbols hex
   end
 end
